bfea4f402c3c48a294da4e35456ffa4182ee11cc
Kyle Chan <kyleschan@yahoo.com>
2020-11-27 22:23:55 -0800

    v1.1

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
+# external CSS stylesheets
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
 app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.SLATE],
+                external_stylesheets=[dbc.themes.SLATE, external_stylesheets],
                 meta_tags=[
                     {
                         'name': 'description',
@@ -50,30 +61,47 @@ positions = [(1, 'GKP'),
 
 app.layout = html.Div([
     html.H1('2-D Player Comparison',
-            style={'text-align': 'center'}),
+            style={'text-align': 'center',
+                   'fontWeight': 'bold'}),
     html.Br(),
     html.Div([
         dcc.Markdown("""
-            **Purpose**
+            ** Purpose **
 
             A Fantasy Premier League (FPL) dashboard
             which allows users to plot several key
             performance metrics against each other
             which allows users to obtain key
             insights on the statistical indicators of top
-            players in the Premier League (EPL).
+            players in the Premier League (EPL).  Inform your
+            next transfer by plotting any of the 50 different
+            metrics, including [xG, xA](https://www.optasports.com/services/analytics/advanced-metrics/),
+            and [VAPM](https://www.reddit.com/r/FantasyPL/comments/6r60fu/exploring_a_key_metric_value_added_per_1m/)
+            to help discern high-value players from the rest of
+            the pack.
+
+            ** Features **
+            - Filter the plot by maximum price, team, and
+            position to find the player that best suits your team's
+            needs.
+            - Hover over points to reveal that individual player's
+            exact values for the selected metrics.
+            - Click and drag to zoom in on a portion of the graph;
+            double-click to zoom out.
+            - Select the Pan icon at the top right of the graph to
+            toggle panning.
         """),
     ],
     style={'display': 'block',
            'marginLeft': 'auto',
            'marginRight': 'auto',
-           'width': '25%'
+           'width': '75%'
     }),
     html.Br(),
     dcc.Graph(id='metric-graph',
               responsive=True,
               config={
-                  'scrollZoom': True,
+                  'scrollZoom': False,
                   'modeBarButtonsToRemove': [
                       'select2d',
                       'lasso2d',
@@ -130,6 +158,19 @@ app.layout = html.Div([
         html.Div([
             html.H2('Filters',
             style={'text-align': 'center'}),
+            html.P('Max Price (inclusive)'),
+            dcc.Dropdown(
+                id='price-cap',
+                options=[{'label': i, 'value': i} for i in np.round(np.arange(4., 13., 0.1), 1)],
+                clearable=True,
+                searchable=False,
+                persistence_type='session',
+                persistence=True,
+                style={'background-color': '#52575C',
+                       'display': 'block',
+                       'margin': 'auto',
+                       'position': 'relative'}
+            ),
             html.P('Team Filter'),
             dcc.Dropdown(
                 id='team-filter',
@@ -154,6 +195,7 @@ app.layout = html.Div([
                 persistence_type='session',
                 persistence=True,
                 style={'background-color': '#52575C',
+                       'display': 'block',
                        'position': 'relative'}
             )
         ],
@@ -171,9 +213,10 @@ app.layout = html.Div([
            'flex-direction': 'row',
            'float': 'center',
            'align-items': 'center',
-           'position': 'relative',}
+           'position': 'relative'}
     ),
 ],
+style={'overflow': 'scroll'}
 )
 
 
@@ -181,19 +224,19 @@ app.layout = html.Div([
     Output('metric-graph', 'figure'),
     [Input('xaxis-column', 'value'),
      Input('yaxis-column', 'value'),
+     Input('price-cap', 'value'),
      Input('team-filter', 'value'),
      Input('position-filter', 'value')])
 def update_graph(xaxis,
                  yaxis,
+                 price_cap,
                  team_filter,
                  pos_filter):
-    if team_filter:
-        filtered_df = df[df['Team'].isin(team_filter)]
-    else:
-        filtered_df = df
 
-    if pos_filter:
-        filtered_df = filtered_df[filtered_df['Position'].isin(pos_filter)]
+    # Apply filters
+    filtered_df = df[(df['Cost'] / 10) <= price_cap] if price_cap else df
+    filtered_df = filtered_df[filtered_df['Team'].isin(team_filter)] if team_filter else filtered_df
+    filtered_df = filtered_df[filtered_df['Position'].isin(pos_filter)] if pos_filter else filtered_df
 
     fig = px.scatter(filtered_df,
                      x=xaxis,
